d55368a444991ffd4faf1715cd0d18a257ad16bf
Cinthia Cabero Mencia <cinthiacm@gmail.com>
2025-10-05 09:14:10 -0300

    SW code paused

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -15,7 +15,7 @@ createRoot(document.getElementById("root")).render(
   </StrictMode>
 );
 
-/* if ("serviceWorker" in navigator) {
+/*  if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
       .register("/sw.js")
